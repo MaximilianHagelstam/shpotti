a1292baaf98ea2769f89f3b5979474e464d4b0da
Maximilian Hagelstam <maximilian.hagelstam@gmail.com>
2022-03-24 17:47:10 +0200

    Remove next api and fix some bugs

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,5 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
-import "../styles/globals.css";
 import theme from "../theme";
 
 function MyApp({ Component, pageProps }: AppProps) {

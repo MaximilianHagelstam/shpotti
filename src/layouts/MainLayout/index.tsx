2cda1a65483e5a00d68f6aa3a33a4bfdad515b80
Maximilian Hagelstam <maximilian.hagelstam@gmail.com>
2022-03-29 22:30:05 +0300

    Create main layout

---
+++ src/layouts/MainLayout/index.tsx
@@ -0,0 +1,18 @@
+import Footer from "../../components/Footer";
+import Header from "../../components/Header";
+
+interface MainLayoutProps {
+  children: React.ReactNode;
+}
+
+const MainLayout = ({ children }: MainLayoutProps) => {
+  return (
+    <>
+      <Header />
+      {children}
+      <Footer />
+    </>
+  );
+};
+
+export default MainLayout;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,12 @@
 import type { NextPage } from "next";
-import Footer from "../components/Footer";
-import Header from "../components/Header";
 import Spots from "../components/Spots";
+import MainLayout from "../layouts/MainLayout";
 
 const Home: NextPage = () => {
   return (
-    <>
-      <Header />
+    <MainLayout>
       <Spots />
-      <Footer />
-      {/* <h1 className="text-6xl font-bold text-center py-12">James</h1> */}
-    </>
+    </MainLayout>
   );
 };
 

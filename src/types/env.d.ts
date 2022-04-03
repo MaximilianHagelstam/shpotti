6f2f1b39b32b125c245f1431a0164d4672b744d8
Maximilian Hagelstam <maximilian.hagelstam@gmail.com>
2022-04-03 15:14:35 +0300

    Connect to firebase

--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -2,7 +2,13 @@ declare namespace NodeJS {
   interface ProcessEnv extends NodeJS.ProcessEnv {
     GOOGLE_ID: string;
     GOOGLE_SECRET: string;
-    NEXTAUTH_URL: string;
     SECRET: string;
+    NEXTAUTH_URL: string;
+    API_KEY: string;
+    AUTH_DOMAIN: string;
+    PROJECT_ID: string;
+    STORAGE_BUCKET: string;
+    MESSAGING_SENDER_ID: string;
+    APP_ID: string;
   }
 }
